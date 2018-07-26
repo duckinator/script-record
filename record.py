9d7a58563d31d72ab816452f9d4eb7c9176319ab
Ellen Marie Dash <me@duckie.co>
2018-07-26 00:58:20 -0400

    *shrugs*

--- record.py
+++ record.py
@@ -36,8 +36,8 @@ def urxvt_dimensions(display, pid):
     env = {"DISPLAY": display}
     win_id = run(["xdotool", "search", "--pid", str(pid)], env=env).strip()
     lines = run(["xwininfo", "-display", display, "-id", win_id]).splitlines()
+    lines = filter(lambda line: ":" in line, lines)
     lines = map(lambda line: normalize_line(line), lines)
-    lines = filter(lambda line: len(line) == 2, lines)
     stats = dict(lines)
     return "x".join([stats["width"], stats["height"]])
 
