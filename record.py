fed9a0050a1884295d7c0438dcf4b5d990d779d8
Ellen Marie Dash <me@duckie.co>
2018-07-26 01:11:04 -0400

    *shrugs forever*

--- record.py
+++ record.py
@@ -12,17 +12,17 @@ def fonts(name="fonf", size=16):
     return ("xft:{}:pixelsize={}".format(name, size),
             "xft:{}:bold:pixelsize={}".format(name, size))
 
-def xephyr_run(display):
+def xephyr_start(display):
     # Use dimensions that are (hopefully) larger than urxvt will need.
     dimensions = "1400x1000"
     return Popen(["Xephyr", "-screen", dimensions, "-resizeable", display])
 
-def ffmpeg_run(display, dimensions, duration, output="output.mp4"):
+def ffmpeg_start(display, dimensions, duration, output="output.mp4"):
     return Popen(["ffmpeg", "-video_size", dimensions, "-framerate", "10",
                     "-f", "x11grab", "-t", duration, "-i", display, output],
                  stderr=PIPE)
 
-def urxvt_run(display, script):
+def urxvt_start(display, script):
     font_norm, font_bold = fonts()
     return Popen(["urxvt", "-display", display, "-fn", font_norm,
                     "-fb", font_bold, "-e", script])
@@ -33,12 +33,16 @@ def normalize_line(line):
     parts = map(str.lower, parts)
     return list(parts)
 
-def urxvt_dimensions(display, pid):
-    winid = run(["xdotool", "search", "--pid", str(pid)], display).strip()
-    lines = run(["xwininfo", "-id", winid], display).splitlines()
+def parse_xwininfo(text):
+    lines = text.splitlines()
     lines = filter(lambda line: ":" in line, lines)
     lines = map(lambda line: normalize_line(line), lines)
-    stats = dict(lines)
+    return dict(lines)
+
+def urxvt_dimensions(display, pid):
+    win_id = run(["xdotool", "search", "--pid", str(pid)], display).strip()
+    xwinfo = run(["xwininfo", "-id", win_id], display)
+    stats  = parse_xwininfo(xwinfo)
     return "x".join([stats["width"], stats["height"]])
 
 script = "/home/pup/demo-script/replay.sh"
@@ -47,15 +51,23 @@ script = "/usr/bin/nvim"
 display = ":1"
 duration = "10"
 
-xephyr = xephyr_run(display)
+xephyr = xephyr_start(display)
 sleep(1)
 
-urxvt  = urxvt_run(display, script)
+urxvt  = urxvt_start(display, script)
 sleep(1)
 dimensions = urxvt_dimensions(display, urxvt.pid)
 
-ffmpeg = ffmpeg_run(display, dimensions, duration)
+ffmpeg = ffmpeg_start(display, dimensions, duration)
 
+# Wait for urxvt to finish.
 urxvt.communicate()
+
+# The script is done -- tell ffmpeg to quit.
+ffmpeg.terminate()
+
+# Wait for ffmpeg to finish.
 ffmpeg.communicate()
-xephyr.kill()
+
+# Close Xephyr.
+xephyr.terminate()
--- replay.sh
+++ replay.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 reset
+sleep 3
 scriptreplay -s /home/pup/demo-script/script -t /home/pup/demo-script/timing
