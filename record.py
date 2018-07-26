88aac3330922d0708a5c8ddc87e3b17878e3f278
Ellen Marie Dash <me@duckie.co>
2018-07-25 23:31:20 -0400

    ?????

--- record.py
+++ record.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_output
 from time import sleep
+import sys
+
+def run(cmd):
+    return check_output(cmd, stderr=sys.stderr, encoding="utf-8")
 
 def fonts(name="fonf", size=16):
     return ("xft:{}:pixelsize={}".format(name, size),
@@ -23,15 +27,14 @@ def urxvt_run(display, script):
                     "-fb", font_bold, "-e", script])
 
 def urxvt_dimensions(display):
-    proc = Popen(["xwininfo", "-display", display, "-root", "-children",
-                    "-stats"], stdout=PIPE, encoding="utf-8")
-    stdout, _ = proc.communicate()
-    lines = stdout.splitlines()
-    lines = map(str.strip, lines)
+    stdout = run(["xwininfo", "-display", display, "-root", "-children",
+                    "-stats"])
+    lines = map(str.strip, stdout.splitlines())
     lines = filter(lambda line: ": " in line, lines)
     stats = dict(map(lambda line: line.split(": ", 1), lines))
     width = stats["Width"]
     height = stats["Height"]
+    print("urxvt dimensions = {}x{}".format(width, height))
     return "x".join([width, height])
 
 script = "/home/pup/demo-script/replay.sh"
